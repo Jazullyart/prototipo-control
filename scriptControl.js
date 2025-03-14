8de3d8bb7c67c8dcad32b9589c73a89251823831
Jazully <80542757+Jazullyart@users.noreply.github.com>
2025-03-13 18:40:53 -0600

    [MODIFICA] Empezando a corregir errores lógicos

---
+++ scriptControl.js
@@ -0,0 +1,183 @@
+const datos_display = document.getElementById("txt_cm");
+
+function enviar(comando) {
+  if (comando == "") {
+    referencia.update({
+      espacio: datos_display.value,
+    });
+  } else {
+    referencia.update({
+      espacio: comando,
+    });
+  }
+
+  return comando;
+}
+
+var categoria = 0;
+
+document.getElementById("Categoria1").addEventListener("click", () => {
+  sendCommand(enviar(CambioDeVista.Categoria_1_Opcion_1));
+  categoria = 1;
+  console.log(categoria);
+});
+
+document.getElementById("Categoria2").addEventListener("click", () => {
+  sendCommand(enviar(CambioDeVista.Categoria_2_Opcion_1));
+  categoria = 2;
+  console.log(categoria);
+});
+
+document.getElementById("Categoria3").addEventListener("click", () => {
+  sendCommand(enviar(CambioDeVista.Categoria_3_Opcion_1));
+  categoria = 3;
+  console.log(categoria);
+});
+
+var opcion = 1;
+
+document.getElementById("Adelante").addEventListener("click", () => {
+  switch (categoria) {
+    case 1:
+      switch (opcion) {
+        case 1:
+          sendCommand(enviar(CambioDeVista.Categoria_1_Opcion_1));
+          opcion = 2;
+          break;
+
+        case 2:
+          sendCommand(enviar(CambioDeVista.Categoria_1_Opcion_2));
+          opcion = 3;
+          break;
+
+        case 3:
+          sendCommand(enviar(CambioDeVista.Categoria_1_Opcion_3));
+          opcion = 1;
+          break;
+
+        default:
+          break;
+      }
+      break;
+
+    case 2:
+      switch (opcion) {
+        case 1:
+          sendCommand(enviar(CambioDeVista.Categoria_2_Opcion_1));
+          opcion = 2;
+          break;
+
+        case 2:
+          sendCommand(enviar(CambioDeVista.Categoria_2_Opcion_2));
+          opcion = 3;
+          break;
+
+        case 3:
+          sendCommand(enviar(CambioDeVista.Categoria_2_Opcion_3));
+          opcion = 1;
+          break;
+
+        default:
+          break;
+      }
+      break;
+
+    case 3:
+      switch (opcion) {
+        case 1:
+          sendCommand(enviar(CambioDeVista.Categoria_3_Opcion_1));
+          opcion = 2;
+          break;
+
+        case 2:
+          sendCommand(enviar(CambioDeVista.Categoria_3_Opcion_2));
+          opcion = 3;
+          break;
+
+        case 3:
+          sendCommand(enviar(CambioDeVista.Categoria_3_Opcion_3));
+          opcion = 1;
+          break;
+
+        default:
+          break;
+      }
+      break;
+
+    default:
+      break;
+  }
+});
+
+document.getElementById("Atras").addEventListener("click", () => {
+  switch (categoria) {
+    case 1:
+      switch (opcion) {
+        case 1:
+          sendCommand("composition/columns/3/connect");
+          opcion = 2;
+          break;
+
+        case 2:
+          sendCommand("composition/columns/1/connect");
+          opcion = 3;
+          break;
+
+        case 3:
+          sendCommand("composition/columns/2/connect");
+          opcion = 1;
+          break;
+
+        default:
+          break;
+      }
+      break;
+
+    case 2:
+      switch (opcion) {
+        case 1:
+          sendCommand("composition/columns/6/connect");
+          opcion = 2;
+          break;
+
+        case 2:
+          sendCommand("composition/columns/4/connect");
+          opcion = 3;
+          break;
+
+        case 3:
+          sendCommand("composition/columns/5/connect");
+          opcion = 1;
+          break;
+
+        default:
+          break;
+      }
+      break;
+
+    case 3:
+      switch (opcion) {
+        case 1:
+          sendCommand("composition/columns/9/connect");
+          opcion = 2;
+          break;
+
+        case 2:
+          sendCommand("composition/columns/7/connect");
+          opcion = 3;
+          break;
+
+        case 3:
+          sendCommand("composition/columns/8/connect");
+          opcion = 1;
+          break;
+
+        default:
+          break;
+      }
+      break;
+
+    default:
+      break;
+  }
+});
---
+++ scriptReceptor.js
@@ -0,0 +1,33 @@
+let CambioDeVista = {
+    "composition/columns/1/connect": "composition/columns/1/connect",
+    "composition/columns/2/connect": "composition/columns/2/connect",
+    "composition/columns/3/connect": "composition/columns/3/connect",
+    "composition/columns/4/connect": "composition/columns/4/connect",
+    "composition/columns/5/connect": "composition/columns/5/connect",
+    "composition/columns/6/connect": "composition/columns/6/connect",
+    "composition/columns/7/connect": "composition/columns/7/connect",
+    "composition/columns/8/connect": "composition/columns/8/connect",
+    "composition/columns/9/connect": "composition/columns/9/connect",
+  };
+
+  
+const datos_display = document.getElementById("txt_cm");
+
+firestore
+.collection("comandos_receptor")
+.doc("comando_captado")
+.onSnapshot((doc) => {
+  console.log(`Detalles: ${doc.data().espacio}`);
+  al_cambiar_el_comando(doc.data().espacio);
+});
+
+function al_cambiar_el_comando(el_comando_recibido) {
+if (CambioDeVista[el_comando_recibido] != undefined) {
+  console.log("El comando es: " + CambioDeVista[el_comando_recibido]);
+  datos_display.addEventListener("load", () => {
+    enviar(el_comando_recibido);
+  });
+} else {
+  alert("Lo que se envio no es un comando reconocido");
+}
+}
